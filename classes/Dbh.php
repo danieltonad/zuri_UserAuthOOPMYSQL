51973aa4e81fa961b31a3c100d5c5becd8bc588a
Solarin Daniel <soladan2000@gmail.com>
2022-06-25 15:08:03 +0100

    Boiler Plate

---
+++ classes/Dbh.php
@@ -0,0 +1,15 @@
+<?php
+class Dbh{
+    private $host;
+    private $database;
+    private $username;
+    private $password;
+
+    public function __construct()
+    {
+        
+    }
+}
+?>
+
+
